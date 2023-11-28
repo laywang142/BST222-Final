9acd62da01a0b0d4151410960c45d8e4d273ec9d
laywang142 <122498550+laywang142@users.noreply.github.com>
2023-11-28 15:34:33 -0800

    data descriptions

--- Read_data.R
+++ Read_data.R
@@ -1,3 +1,11 @@
 library(KMsurv)
 ### We're using Bfeed from KMsurv
 data(bfeed)
+
+#in general, for this dataset, 1 = yes
+
+#delta <- indicator for if the kid got off breastfeeding
+#ybirth <- year of birth for kid
+#yschool <- mother's education level
+#pc3mth <- Prenatal care after 3rd month (1= yes)
+
